435613e38c117594378e819f3233089ca52bcd69
Behrouz Akhbari <akhbaribeh@gmail.com>
2022-04-04 10:10:28 -0400

    Add production build

---
+++ build/precache-manifest.b35f786c00ec791f4fad014c29d930bc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2087741674a2ba04c7add193601073c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f51b80793d76014c617d",
+    "url": "/static/css/2.4253ffa8.chunk.css"
+  },
+  {
+    "revision": "6b5c48edcfba1606fc1e",
+    "url": "/static/css/main.529e62a6.chunk.css"
+  },
+  {
+    "revision": "f51b80793d76014c617d",
+    "url": "/static/js/2.d72a35b2.chunk.js"
+  },
+  {
+    "revision": "b9325bd15ee10c9d22b440ba64b07529",
+    "url": "/static/js/2.d72a35b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5c48edcfba1606fc1e",
+    "url": "/static/js/main.8552250e.chunk.js"
+  },
+  {
+    "revision": "cc454f581cdc072df54e",
+    "url": "/static/js/runtime-main.4c381e45.js"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/static/media/bootstrap.73844123.png"
+  },
+  {
+    "revision": "b4dd4c5e18b3c0a0c35018f37c2b2f94",
+    "url": "/static/media/c.b4dd4c5e.png"
+  },
+  {
+    "revision": "bf2ebe6a4a2f42096313fa94c7ba2030",
+    "url": "/static/media/css.bf2ebe6a.png"
+  },
+  {
+    "revision": "2d78f49311cf0c079ea8aa1cae1a9de4",
+    "url": "/static/media/git.2d78f493.png"
+  },
+  {
+    "revision": "652fdb659a681116811612e0b9e25354",
+    "url": "/static/media/java.652fdb65.png"
+  },
+  {
+    "revision": "d3304b833b96ca9b6b4d4f12d07d7176",
+    "url": "/static/media/js.d3304b83.png"
+  },
+  {
+    "revision": "07c461c758678d450009656da90a9f43",
+    "url": "/static/media/musketeers.07c461c7.PNG"
+  },
+  {
+    "revision": "a124d8c48f53b5776cca71393d406f0d",
+    "url": "/static/media/mysh.a124d8c4.PNG"
+  },
+  {
+    "revision": "48c9f0911e598e8312494409d4b81148",
+    "url": "/static/media/myweb.48c9f091.PNG"
+  },
+  {
+    "revision": "4c6c0929cc58f241847a744b2de75e2f",
+    "url": "/static/media/nodejs.4c6c0929.png"
+  },
+  {
+    "revision": "22eeecf99b8d92726b4f3735be5473c0",
+    "url": "/static/media/python.22eeecf9.png"
+  },
+  {
+    "revision": "cf9d26c3c13653602c28a7dd0a75d123",
+    "url": "/static/media/react.cf9d26c3.png"
+  },
+  {
+    "revision": "715313cd4f8b7b826663309b8751bb6b",
+    "url": "/static/media/svg-1.715313cd.svg"
+  },
+  {
+    "revision": "d9e440a5ed9283a035d51b4cb93663f0",
+    "url": "/static/media/svg-3.d9e440a5.svg"
+  },
+  {
+    "revision": "494b2abdfb54672b38d5c6135a33eda5",
+    "url": "/static/media/svg-5.494b2abd.svg"
+  },
+  {
+    "revision": "3ba145f994e790567bea8bf3b6c3b933",
+    "url": "/static/media/trie2.3ba145f9.PNG"
+  },
+  {
+    "revision": "72296f0bbb2226204d0760d642207e52",
+    "url": "/static/media/video.72296f0b.mp4"
+  }
+]);
